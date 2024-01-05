b82870e9531914ce28136982e5398c729bb7255b
tsukaryu <drgon8otsuka@gmail.com>
2024-01-05 12:08:02 +0900

    finish ch 8

--- src/tool/GenerateAst.java
+++ src/tool/GenerateAst.java
@@ -22,6 +22,7 @@ public class GenerateAst {
         "Variable: Token name"));
 
     defineAst(outputDir, "Stmt", Arrays.asList(
+        "Block : List<Stmt> statements",
         "Expression : Expr expression",
         "Print : Expr expressions",
         "Var : Token name, Expr initializer"));
