022908e25b3a18b68281edfa3433cf30c379e1d8
tsukaryu <drgon8otsuka@gmail.com>
2024-01-05 20:07:47 +0900

    wip: p191 instanceとarityチェック

---
+++ src/lox/LoxCallable.java
@@ -0,0 +1,12 @@
+package src.lox;
+
+import java.util.List;
+
+/**
+ * LoxCallable
+ */
+public interface LoxCallable {
+  int arity();
+
+  Object call(Interpreter interpreter, List<Object> arguments);
+}
