b1e8a12d0cdcd62c8abfc60b304151991b63edbb
tsukaryu <drgon8otsuka@gmail.com>
2024-01-05 18:46:39 +0900

    wip: p171 if stmt

--- src/lox/Interpreter.java
+++ src/lox/Interpreter.java
@@ -126,6 +126,16 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
   }
 
   @Override
+  public Void visitIfStmt(Stmt.If stmt) {
+    if (isTruthy(evaluate(stmt.condition))) {
+      execute(stmt.theBranch);
+    } else if (stmt.elseBranch != null) {
+      execute(stmt.elseBranch);
+    }
+    return null;
+  }
+
+  @Override
   public Void visitPrintStmt(Stmt.Print stmt) {
     Object value = evaluate(stmt.expressions);
     System.out.println(stringify(value));
--- src/tool/GenerateAst.java
+++ src/tool/GenerateAst.java
@@ -24,6 +24,7 @@ public class GenerateAst {
     defineAst(outputDir, "Stmt", Arrays.asList(
         "Block : List<Stmt> statements",
         "Expression : Expr expression",
+        "If : Expr condition, Stmt theBranch," + " Stmt elseBranch",
         "Print : Expr expressions",
         "Var : Token name, Expr initializer"));
   }
