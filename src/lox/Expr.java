e10c427959b03689215eaa0e0aa93a5a5cf48ed7
tsukaryu <drgon8otsuka@gmail.com>
2024-01-05 11:48:08 +0900

    wip: ここらへんよくわからないけど、代入できるようになったらしい

--- src/lox/Expr.java
+++ src/lox/Expr.java
@@ -1,18 +1,30 @@
 package src.lox;
 
+import java.util.List;
+
 abstract class Expr {
   interface Visitor<R> {
+    R visitAssignExpr(Assign expr);
     R visitBinaryExpr(Binary expr);
-
     R visitGroupingExpr(Grouping expr);
-
     R visitLiteralExpr(Literal expr);
-
     R visitUnaryExpr(Unary expr);
-
     R visitVariableExpr(Variable expr);
   }
+  static class Assign extends Expr {
+    Assign(Token name, Expr value) {
+      this.name = name;
+      this.value = value;
+    }
 
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitAssignExpr(this);
+    }
+
+    final Token name;
+    final Expr value;
+  }
   static class Binary extends Expr {
     Binary(Expr left, Token operator, Expr right) {
       this.left = left;
@@ -29,7 +41,6 @@ abstract class Expr {
     final Token operator;
     final Expr right;
   }
-
   static class Grouping extends Expr {
     Grouping(Expr expression) {
       this.expression = expression;
@@ -42,7 +53,6 @@ abstract class Expr {
 
     final Expr expression;
   }
-
   static class Literal extends Expr {
     Literal(Object value) {
       this.value = value;
@@ -55,7 +65,6 @@ abstract class Expr {
 
     final Object value;
   }
-
   static class Unary extends Expr {
     Unary(Token operator, Expr right) {
       this.operator = operator;
@@ -70,7 +79,6 @@ abstract class Expr {
     final Token operator;
     final Expr right;
   }
-
   static class Variable extends Expr {
     Variable(Token name) {
       this.name = name;
