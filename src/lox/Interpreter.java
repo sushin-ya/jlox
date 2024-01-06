b3c3102141dcad39b81737ee1e170282363d9941
tsukaryu <drgon8otsuka@gmail.com>
2024-01-06 12:58:34 +0900

    wip p247 getter

--- src/lox/Interpreter.java
+++ src/lox/Interpreter.java
@@ -313,4 +313,14 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     }
     return function.call(this, arguments);
   }
+
+  @Override
+  public Object visitGetExpr(Expr.Get expr) {
+    Object object = evaluate(expr.object);
+    if (!(object instanceof LoxInstance)) {
+      return ((LoxInstance) object).get(expr.name);
+    }
+
+    throw new RuntimeError(expr.name, "Only instances have properties");
+  }
 }
